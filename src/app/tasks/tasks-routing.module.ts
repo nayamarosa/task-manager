2ce9b02d2d933addf36d865f4f13d66c0182f154
Nayama Rosa <falecom@nayama.com.br>
2025-05-13 11:08:44 -0300

    adjusting html in task-list. Remove tasks component from tasks-routing and tasks module

--- src/app/tasks/tasks-routing.module.ts
+++ src/app/tasks/tasks-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TasksComponent } from './tasks.component';
 import { TaskFormComponent } from './task-form/task-form.component';
+import { TaskListComponent } from './task-list/task-list.component';
 
 const routes: Routes = [
-  { path: '', component: TasksComponent },
+  { path: '', component: TaskListComponent },
   { path: 'new', component: TaskFormComponent },
 ];
 
--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TasksRoutingModule } from './tasks-routing.module';
-import { TasksComponent } from './tasks.component';
 import { TaskListComponent } from './task-list/task-list.component';
 import { TaskFormComponent } from './task-form/task-form.component';
 
 
 @NgModule({
   declarations: [
-    TasksComponent,
     TaskListComponent,
     TaskFormComponent
   ],
