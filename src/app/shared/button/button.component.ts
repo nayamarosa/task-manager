0d8c216ba3eb38b62d2e646edfcbfe6c4c90760d
Nayama Rosa <falecom@nayama.com.br>
2025-05-13 11:37:10 -0300

    creating button component

---
+++ src/app/shared/button/button.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  standalone: false,
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss'
+})
+
+export class ButtonComponent {
+  @Input() text: string = '';
+  @Input() icon: string = '';
+  @Input() disabled: boolean = false;
+  @Input() type: 'primary' | 'icon' | '' = 'primary';
+}
