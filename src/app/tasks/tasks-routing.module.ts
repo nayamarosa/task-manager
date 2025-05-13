d1a9a32af6988070d3c4cead4689fafc99aaf1c9
Nayama Rosa <falecom@nayama.com.br>
2025-05-13 10:40:27 -0300

    creating tasks/new route

--- src/app/tasks/tasks-routing.module.ts
+++ src/app/tasks/tasks-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TasksComponent } from './tasks.component';
+import { TaskFormComponent } from './task-form/task-form.component';
 
-const routes: Routes = [{ path: '', component: TasksComponent }];
+const routes: Routes = [
+  { path: '', component: TasksComponent },
+  { path: 'new', component: TaskFormComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
