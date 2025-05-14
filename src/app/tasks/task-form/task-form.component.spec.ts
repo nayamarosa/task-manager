dd0ce69f4b7e75d11d09855b142e9e4d49bf311c
Nayama Rosa <falecom@nayama.com.br>
2025-05-14 14:23:21 -0300

    updating task form test

--- src/app/tasks/task-form/task-form.component.spec.ts
+++ src/app/tasks/task-form/task-form.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TaskFormComponent } from './task-form.component';
+import { ButtonComponent } from '../../shared/button/button.component';
+import { InputComponent } from '../../shared/input/input.component';
+import { TextareaComponent } from '../../shared/textarea/textarea.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('TaskFormComponent', () => {
   let component: TaskFormComponent;
@@ -8,7 +12,8 @@ describe('TaskFormComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [TaskFormComponent]
+      declarations: [TaskFormComponent, ButtonComponent, InputComponent, TextareaComponent],
+      imports: [ReactiveFormsModule],
     })
     .compileComponents();
 
